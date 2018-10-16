8beba58b7092c3979fcb007d5be85ba60c19e316
Jacob Evans <jtevans.94@gmail.com>
2018-10-16 15:17:04 -0400

    changed theme, renamed with DuDe

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Computational Boot Camp'
+project = 'DuDe Computational Boot Camp'
 copyright = '2018, Jacob Evans'
 author = 'Jacob Evans'
 
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -100,7 +100,7 @@ html_static_path = ['.static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ComputationalBootCampdoc'
+htmlhelp_basename = 'DuDeComputationalBootCampdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -127,7 +127,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ComputationalBootCamp.tex', 'Computational Boot Camp Documentation',
+    (master_doc, 'DuDeComputationalBootCamp.tex', 'DuDeComputational Boot Camp Documentation',
      'Jacob Evans', 'manual'),
 ]
 
@@ -137,7 +137,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'computationalbootcamp', 'Computational Boot Camp Documentation',
+    (master_doc, 'dudecomputationalbootcamp', 'DuDe Computational Boot Camp Documentation',
      [author], 1)
 ]
 
@@ -148,8 +148,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ComputationalBootCamp', 'Computational Boot Camp Documentation',
-     author, 'ComputationalBootCamp', 'One line description of project.',
+    (master_doc, 'DuDeComputationalBootCamp', 'DuDe Computational Boot Camp Documentation',
+     author, 'DuDeComputationalBootCamp', 'One line description of project.',
      'Miscellaneous'),
 ]
 
